79d8eaa4d7fee4b28c63eaa8d4d717b9ce0fbd3a
Nathan Bouchez <nbouchez@e-r4-p4.s19.be>
2018-11-15 05:28:28 +0100

    save_15/11/18

--- Fillit/fillit.c
+++ Fillit/fillit.c
@@ -18,8 +18,55 @@ void	usage()
 }
 
 /*	
-**	input : adresse du tableau, taille carre, Adresse du tableau des elements, adresse de la 1er piece
+**	input : adresse du tableau de piece, taille du tableau
 **	output : -1 = impossible de placer une piece | 0 = Besoin de plus de place | 1 = Succes une piece | 2 = Succes end
+*/
+
+void affichage(char *workspace, int tab_size)
+{
+	int col;
+	int lig;
+
+	lig = 0;
+	printf("\n\t [ Workspace ]\n");
+	printf("\tSize : %d\n", tab_size);
+	printf("+");
+	while(lig < tab_size + 1)
+	{
+		printf("-");
+		lig++;
+	}
+	printf("+\n");
+	lig = 0;
+	while(lig < tab_size)
+	{
+		col = 0;
+		printf("|");
+		while(col < tab_size + 1)
+		{
+			if((*workspace <= 'Z' && *workspace >= 'A') || *workspace == -1)
+				printf("%c", *workspace);
+			else
+				printf("*");
+			workspace++;
+			col++;
+		}
+		printf("|\n");
+		lig++;
+	}
+	printf("+");
+	lig = 0;
+	while(lig < tab_size + 1)
+	{
+		printf("-");
+		lig++;
+	}
+	printf("+\n");
+}
+
+/*	
+**	input : adresse du tableau, taille carre, Adresse du tableau des elements, adresse de la 1er piece
+**	output : -1 = impossible de placer la piece | 0 = Besoin de plus de place | 1 = Succes une piece | 2 = Succes end
 **
 **
 */
@@ -28,49 +75,54 @@ int enfiler(char *workspace, int tab_size, int *index, t_coord **piece)
 {
 	int result;
 	int index_origin;
+	int offset;
 
-	result = 1;
+	result = -1;
 	index_origin = 0;
-	while(index_origin < tab_size)
+	printf(".. Nouvelles piece\n");
+	//affichage(workspace, tab_size);
+	while(index_origin != tab_size * tab_size)
 	{
 		if(workspace[index_origin] == '\0')
 		{
-			if(workspace[index_origin + piece[0]->x + (piece[0]->y) * (-tab_size)] == '\0')
+			if(workspace[index_origin + piece[0]->x + (piece[0]->y) * (-tab_size)] == '\0') //Ajoute de x + ajout de y (lignes) * tab_size + 1
 			{
 				if(workspace[index_origin + piece[1]->x + (piece[1]->y) * (-tab_size)] == '\0')
 				{
 					if(workspace[index_origin + piece[2]->x + (piece[2]->y) * (-tab_size)] == '\0')
 					{
-						printf("Ca parait bon");
+						printf("J'ai trouve une place libre\n");
 						workspace[index_origin] = 'A';
-						workspace[index_origin + piece[0]->x + (piece[0]->y) * (-tab_size)] = 'B';
-						workspace[index_origin + piece[1]->x + (piece[1]->y) * (-tab_size)] = 'C';
-						workspace[index_origin + piece[2]->x + (piece[2]->y) * (-tab_size)] = 'D';
-						index_origin = tab_size;
+						workspace[index_origin + piece[0]->x + (piece[0]->y) * (-tab_size)] = 'A';
+						workspace[index_origin + piece[1]->x + (piece[1]->y) * (-tab_size)] = 'A';
+						workspace[index_origin + piece[2]->x + (piece[2]->y) * (-tab_size)] = 'A';
+						if(piece[3])
+						{
+							printf("Debut de la piece suivante\n");
+							result = enfiler(workspace, tab_size, index, piece + 3);
+							printf("1\n");
+						}
+						else
+						{
+							printf("Pas de piece suivante");
+							return 2;
+						}
 					}
 				}
 			}
-
 		}
 		index_origin++;
 	}
-	index_origin = 0;
-	printf("Tableau \n");
-	while(index_origin < tab_size)//Affichage
+	if(index_origin == tab_size * tab_size)
 	{
-		printf(" %c ", workspace[index_origin]);
-		index_origin++;
+		return 0;
 	}
-
-	if(result == 1)
+	if(result == -1)
 	{
-		return (1);
+		result = enfiler(workspace, tab_size, index, piece + 3);
 	}
-	else
-	{
-		result = enfiler(workspace, tab_size, index, piece);
-	}
-
+	affichage(workspace, tab_size - 1);
+	printf("\nResult : %d\n", result);
 	return (result);
 }
 
@@ -80,8 +132,10 @@ int fillit(t_coord **tab, int nb_piece)
 	int end;
 	int tab_size;
 	int index[26];
+	int i;
 
-	tab_size = 2;
+	i = 0;
+	tab_size = 5;
 	end = 0;
 	workspace = NULL;
 	if(tab == NULL)
@@ -89,11 +143,17 @@ int fillit(t_coord **tab, int nb_piece)
 	while(end == 0)
 	{
 		free(workspace);
-		if((workspace = (char *)ft_strnew(tab_size * tab_size)) == NULL)
+		if((workspace = (char *)ft_strnew(tab_size * tab_size + tab_size)) == NULL)
 			return (0);
-		end = enfiler(workspace , tab_size * tab_size, &index[0], tab);
-		tab_size++;
+		while(i < tab_size)
+		{
+			workspace[3 + i * (tab_size+1)] = (char)-1; //Creation de case morte
+			i++;
+		}
+		end = enfiler(workspace , tab_size + 1, &index[0], tab);
+		//tab_size++;
 	}
+	//affichage(workspace, tab_size);
 	return (0);
 }
 
@@ -124,11 +184,12 @@ void generateur_piece(t_coord **tab, int nb_piece)
 	printf("Piece %d : %p\n", i/3+1, tab[i]);
 }
 
+
 int main(int argc, char const *argv[])
 {
 	t_coord coord;
 	t_coord *tab_coord[78];//Pour exemple
-	int nb_piece = 1;
+	int nb_piece = 2;
 	printf("%p\n", &tab_coord[0]);
 	generateur_piece(tab_coord, nb_piece);
 
@@ -143,3 +204,4 @@ int main(int argc, char const *argv[])
 
 	return (0);
 }
+
